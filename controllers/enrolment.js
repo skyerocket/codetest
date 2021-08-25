53b18a022edc64965248d1a2bdf4091e20d78d0d
Yang Fei <yangfei9310@gmail.com>
2021-08-25 20:17:19 +1000

    put in one place

--- controllers/enrolment.js
+++ controllers/enrolment.js
@@ -1,9 +1,47 @@
 module.exports = (db) => {
-    const { EnrolmentModel } = require('../models')(db);
-    const TeacherController  = require('./teacher');
-    const StudentController = require('./student');
-    
+    const { StudentModel, EnrolmentModel, TeacherModel } = require('../models')(db);
+
     let EnrolmentController = {};
+
+    /**
+	 * Get student by email
+	 * @param {string} email
+	 * @returns {Promise<any>}
+	 */
+    EnrolmentController.getStudentByEmail = async (email) => {
+		const student = await StudentModel.findOne({ where: { email } });
+		return student;
+	};
+
+	/**
+	 * Get students by emails
+	 * @param {Array} emails
+	 * @returns {Promise<any>}
+	 */
+    EnrolmentController.getStudentsByEmails = async (emails) => {
+		const students = await StudentModel.findAll({ where: { email: emails } });
+		return students;
+	};
+
+    /**
+	 * Get teacher by email
+	 * @param {string} email
+	 * @returns {Promise<any>}
+	 */
+	EnrolmentController.getTeacherByEmail = async (email) => {
+		const teacher = await TeacherModel.findOne({ where: { email } });
+		return teacher;
+	};
+
+	/**
+	 * Get teachers by emails
+	 * @param {Array} emails
+	 * @returns {Promise<any>}
+	 */
+	EnrolmentController.getTeachersByEmails = async (emails) => {
+		const teachers = await TeacherModel.findAll({ where: { email: emails } });
+		return teachers;
+	};
 
     /**
 	 * Register students to teacher
@@ -12,11 +50,9 @@ module.exports = (db) => {
 	 * @returns {Promise<void>}
 	 */
     EnrolmentController.registerStudentsToTeacher = async (teacherEmail, studentEmails) => {
-        const teacher = await TeacherController.getByEmail(teacherEmail);
+        const teacher = await EnrolmentController.getTeacherByEmail(teacherEmail);
         console.log(teacher)
-        studentEmails.forEach(async email => {
-            const student = await StudentController.getByEmail(email);
-        });
+        const students = await EnrolmentController.getStudentsByEmails(studentEmails);
     };
 
     return EnrolmentController;
